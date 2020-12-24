84a9e2e4bcf59deddf0e3e2740cc113766def8c1
johncrawley <johncrawley1@gmail.com>
2020-12-24 17:46:47 +0000

    Created config for the JMS server and task scheduling, and a message class

---
+++ src/main/java/com/jacstuff/msscjms/model/HelloWorldMessage.java
@@ -0,0 +1,23 @@
+package com.jacstuff.msscjms.model;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+	static final long serialVersionUID = -6123134324L;
+	private UUID id;
+	private String message;
+	
+
+
+}
